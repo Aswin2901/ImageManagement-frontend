f754958eef81e2900be1af6b749588ec042da871
Aswin v gopal <aswinvazhathodiyil113@gmail.com>
2025-01-25 20:22:24 +0530

    updated with new render link

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -43,7 +43,7 @@ const DraggableImage = ({ image, index, moveImage, handleUpdate, handleDelete })
   {!isEditing ? (
     <>
       <img
-        src={`http://127.0.0.1:8000${image.image}`}
+        src={`https://imagemanagement-tymu.onrender.com${image.image}`}
         alt={image.title}
       />
       <p>{image.title}</p>
@@ -59,7 +59,7 @@ const DraggableImage = ({ image, index, moveImage, handleUpdate, handleDelete })
           src={
             editedImage
               ? URL.createObjectURL(editedImage)
-              : `http://127.0.0.1:8000${image.image}`
+              : `https://imagemanagement-tymu.onrender.com${image.image}`
           }
           alt="Preview"
         />
