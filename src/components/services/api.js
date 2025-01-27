51216278141630d351e70c7f9b793dcf6d2a47fc
Aswin v gopal <aswinvazhathodiyil113@gmail.com>
2025-01-27 15:44:20 +0530

    link changed according to the python anywhere

--- src/components/services/api.js
+++ src/components/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/',  // Base API URL
+  baseURL: 'https://aswin2901.pythonanywhere.com/',  // Base API URL
   headers: {
     'Content-Type': 'application/json',
   },
